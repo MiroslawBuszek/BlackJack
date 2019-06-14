1c8d4bd65ee9361c828fadceae7adff46c231001
Mirosław Buszek <zoowett@gmail.com>
2019-06-14 14:44:35 +0200

    Create game bj

---
+++ main.js
@@ -0,0 +1,35 @@
+const suits = ['Diamonds', 'Spades', 'Hearts', 'Clubs'];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+
+function createDeck() {
+  const deck = [];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      let card = {
+        suit: suits[i],
+        value: values[j]
+      };
+      deck.push(card);
+    }
+  }
+  return deck;
+}
+
+function getCardString(card){
+    return card.value + ' of ' + card.suit
+}
+
+function getNextCards() {
+  return deck.shift();
+}
+
+const deck = createDeck()
+
+for (let k = 0; k < deck.length; k++) {
+  console.log(deck[k]);
+}
+
+const playerCards = [getNextCards(), getNextCards()];
+
+console.log(getCardString(playerCards[0]));
+console.log(getCardString(playerCards[1]));
