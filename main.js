64af697c14086ea600f452f0496ec7fd83c6474f
Mirosław Buszek <zoowett@gmail.com>
2019-06-14 15:34:45 +0200

    Add button click

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const suits = ['Diamonds', 'Spades', 'Hearts', 'Clubs'];
 const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
 
+let textArea = document.getElementById('text-area');
+let newGameButton = document.getElementById('new-game-button');
+let hitButton = document.getElementById('hit-button');
+let stayButton = document.getElementById('stay-button');
+
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+
+newGameButton.addEventListener('click', function() {
+  textArea.innerText = 'Started...';
+  newGameButton.style.display = 'none';
+  hitButton.style.display = 'inline';
+  stayButton.style.display = 'inline';
+});
+
 function createDeck() {
   const deck = [];
   for (let i = 0; i < suits.length; i++) {
@@ -15,15 +30,15 @@ function createDeck() {
   return deck;
 }
 
-function getCardString(card){
-    return card.value + ' of ' + card.suit
+function getCardString(card) {
+  return card.value + ' of ' + card.suit;
 }
 
 function getNextCards() {
   return deck.shift();
 }
 
-const deck = createDeck()
+const deck = createDeck();
 
 for (let k = 0; k < deck.length; k++) {
   console.log(deck[k]);
